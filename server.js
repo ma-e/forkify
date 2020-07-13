30477292ad9200fe80603c7738fb0bbf91522996
ma-e <ema@live.nmhu.edu>
2020-07-12 17:41:39 -0700

    add config

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
+var path = require('path');
+var express = require('express');
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+var app = express();
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './src/index.html'));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
 });
-
-app.listen(port);
